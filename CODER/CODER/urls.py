ee52378d301838bef23d1d158815eddf9601a8ec
thomass1510 <91555445+thomass1510@users.noreply.github.com>
2022-07-14 22:09:02 -0300

    url arreglada

--- CODER/CODER/urls.py
+++ CODER/CODER/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppCoder/', include('AppCoder.urls')),
+    path('', include('AppCoder.urls')),
 ]
